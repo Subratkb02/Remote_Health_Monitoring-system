be2dd665a049d79a278b2b364a4aa7feb28799a4
sayanophile <chakrabortysayan922@gmail.com>
2025-05-08 01:14:46 +0530

    Fixed dynamodb connection

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Dashboard from "./Pages/Dashboard";
+import "./css/App.css";
+import AppFieldsProvider from "./GlobalContext/AppContext";
+
+function App() {
+  return (
+    <div className="App">
+      <AppFieldsProvider>
+        <Dashboard />
+      </AppFieldsProvider>
+    </div>
+  );
+}
+
+export default App;
